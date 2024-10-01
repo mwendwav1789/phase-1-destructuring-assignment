a7de89bb0683c4d28f3b54771c1aa275e2c84fed
Victor <mwendwav1789@gmail.com>
2024-10-01 12:07:26 +0300

    update index.html

--- index.js
+++ index.js
@@ -1,28 +1,28 @@
-const farmAnimals = 'cow horse sheep pig chicken';
+const farmAnimals = "cow horse sheep pig chicken";
 
-const colors = ['red', 'orange', 'yellow', 'green', 'blue', 'indigo', 'violet'];
+const colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
 
 const muppet = {
-  muppetName: 'Miss Piggy',
-  color: 'pink',
-  song: 'Never Before, Never Again',
-  job: 'Cast member of The Muppet Show',
-  partner: 'Kermit'
+  muppetName: "Miss Piggy",
+  color: "pink",
+  song: "Never Before, Never Again",
+  job: "Cast member of The Muppet Show",
+  partner: "Kermit",
 };
 
 const nestedMuppet = {
-  nestedName: 'Kermit',
-  nestedColor: 'green',
+  nestedName: "Kermit",
+  nestedColor: "green",
   album: {
     theMuppetMovie: {
-      song1: 'Rainbow Connection',
-      song2: 'Moving Right Along',
-      song3: 'Never Before, Never Again',
-      song4: 'I Hope That Something Better Comes Along',
+      song1: "Rainbow Connection",
+      song2: "Moving Right Along",
+      song3: "Never Before, Never Again",
+      song4: "I Hope That Something Better Comes Along",
     },
   },
-  nestedJob: 'Host of The Muppet Show',
-  nestedPartner: 'Miss Piggy'
+  nestedJob: "Host of The Muppet Show",
+  nestedPartner: "Miss Piggy",
 };
 
 // Strings
@@ -39,10 +39,71 @@ const nestedMuppet = {
 
 // 5. Some people have a really hard time picking out indigo, so let's leave that one out, using the first letter of each color as the variable names.
 
-// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg. 
+// 6. But wait! Indigo is now feeling *super* left out. Let's only assign indigo using indg.
 
 // Objects
 
 // 7. Use destructuring to assign all variables using the keys as the variable names
 
 // 8. Use destructuring to assign songs 2 and 4, and Kermit's job and partner
+
+// Strings
+const animalSounds = {
+  cow: "moo",
+  sheep: "baa",
+  pig: "oink",
+  chicken: "cluck",
+  horse: "neigh",
+};
+const { cow, sheep, pig, chicken } = animalSounds; // horse is excluded
+
+const animals = {
+  bessie: "cow",
+  dolly: "sheep",
+  babe: "pig",
+  little: "chicken",
+};
+const { bessie, dolly, babe, little } = animals; // horse is gone, only four animals
+
+const farmAnimals = {
+  blackAndWhite: "bessie",
+  black: "dolly",
+  pink: "babe",
+};
+const { blackAndWhite, black, pink } = farmAnimals;
+
+// Arrays
+const colors = ["blackAndWhite", "black", "pink"];
+const [blackAndWhiteColor, blackColor, pinkColor] = colors;
+
+const rainbowColors = [
+  "red",
+  "orange",
+  "yellow",
+  "green",
+  "blue",
+  "indigo",
+  "violet",
+];
+const [r, o, y, g, b, , v] = rainbowColors; // indigo is skipped
+const [, , , , , indg] = rainbowColors; // only assign indigo
+
+// Objects
+const person = {
+  name: "Kermit",
+  job: "Host",
+  partner: "Miss Piggy",
+  species: "frog",
+  favoriteSong: "Rainbow Connection",
+};
+const { name, job, partner, species, favoriteSong } = person;
+
+const playlist = [
+  "Bein’ Green",
+  "Rainbow Connection",
+  "It’s Not Easy Bein’ Green",
+  "Movin’ Right Along",
+];
+const [, song2, , song4] = playlist;
+
+const { job: kermitJob, partner: kermitPartner } = person;
